b1cc7fd4ca955c4be5552dfcf0854c50defc7726
Marc Deveaux <mdvx@users.noreply.github.com>
2019-10-29 15:52:27 -0400

    add proxy support

--- src/main/java/com/github/ccob/bittrex4j/HttpFactory.java
+++ src/main/java/com/github/ccob/bittrex4j/HttpFactory.java
@@ -14,19 +14,26 @@ package com.github.ccob.bittrex4j;
 import com.github.signalr4j.client.Logger;
 import com.github.signalr4j.client.Platform;
 import com.github.signalr4j.client.hubs.HubConnection;
+import org.apache.http.HttpHost;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.impl.client.HttpClients;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 public class HttpFactory {
-
-
-
     public HttpClient createClient(){
+        HttpHost proxy = null;
+        if (System.getProperty("http.proxyHost") != null) {
+            proxy = new HttpHost(System.getProperty("http.proxyHost"), Integer.parseInt(System.getProperty("http.proxyPort", "3128")));
+        }
+
         return HttpClients
                 .custom()
-                //hack, since we cant control the user agent inside the signalr library
+                //HACK: since we cant control the user agent inside the signalr library
                 .setUserAgent(Utils.getUserAgent())
+                .setProxy(proxy)
                 .build();
     }
 
